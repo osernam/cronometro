52385acd9db7d47cc5b6723f635facd74632d009
osernam <osernam@gmail.com>
2023-05-04 18:57:01 -0500

    cronometro js

--- cronometro/static/cronometro/cronometro.js
+++ cronometro/static/cronometro/cronometro.js
@@ -1,33 +1,66 @@
 
-var timerId;
-var startTime;
+var tiempoActual;
+var tiempoInicial;
+var tiempoParcial = [];
 
-function startTimer() {
-  startTime = Date.now();
-  timerId = setInterval(updateTimer, 1000);
+
+function iniciarCronometro() {
+    tiempoInicial = Date.now();
+    tiempoActual = setInterval(actualizarTiempo, 100);
+}
+function parcialCronometro() {
+    tiempoParcial.push (Date.now() - tiempoInicial);
+}
+function pararCronometro() {
+    clearInterval(tiempoActual);
 }
 
-function stopTimer() {
-  clearInterval(timerId);
+function actualizarTiempo() {
+    var tiempoTranscurrido = Date.now() - tiempoInicial;
+    var horas = Math.floor(tiempoTranscurrido / 3600000);
+    var minutos = Math.floor((tiempoTranscurrido % 3600000) / 60000);
+    var segundos = Math.floor((tiempoTranscurrido % 60000) / 1000);
+    
+    var centecimasMinuto = Math.floor(tiempoTranscurrido/ 10000);
+    // 24 horas = 10 horas decimales, 1 hora decimal = 100 minutos decimales, 1 minuto decimal = 100 segundos decimales
+
+    var horasDecimales = Math.floor(((tiempoTranscurrido / 3600000)*24)/10);
+    var minutosDecimales = Math.floor((((tiempoTranscurrido / 3600000)*24)/10)*100);
+    var segundosDecimales = Math.floor((((tiempoTranscurrido / 60000)*24)/10)*1000);
+
+    // add leading zeros if necessary
+    if (horas < 10) {
+    horas = '0' + horas;
+    }
+    if (minutos < 10) {
+    minutos = '0' + minutos;
+    }
+    if (segundos < 10) {
+    segundos = '0' + segundos;
+    }
+    if( centecimasMinuto < 10) {
+    centecimasMinuto = '0' + centecimasMinuto;
+    }
+
+    if (horasDecimales < 10) {
+    horasDecimales = '0' + horasDecimales;
+    }
+    if (minutosDecimales < 10) {
+    minutosDecimales = '0' + minutosDecimales;
+    }
+    if (segundosDecimales < 10) {
+    segundosDecimales = '0' + segundosDecimales;
+    }
+
+    var tiempoString = horas + ':' + minutos +  ':' + segundos  + ':' + centecimasMinuto;
+    document.getElementById('tiempo').innerHTML = tiempoString;
+
+    var tiempoDecimal = horasDecimales + ':' + minutosDecimales + ':' + segundosDecimales;
+    document.getElementById('tiempoDecimal').innerHTML = tiempoDecimal;
+    //var timeString = hours + ':' + minutes + ':' + seconds;
+    //document.getElementById('timer').innerHTML = timeString;
 }
 
-function updateTimer() {
-  var elapsedTime = Date.now() - startTime;
-  var hours = Math.floor(elapsedTime / 3600000);
-  var minutes = Math.floor((elapsedTime % 3600000) / 60000);
-  var seconds = Math.floor((elapsedTime % 60000) / 1000);
 
-  // add leading zeros if necessary
-  if (hours < 10) {
-    hours = '0' + hours;
-  }
-  if (minutes < 10) {
-    minutes = '0' + minutes;
-  }
-  if (seconds < 10) {
-    seconds = '0' + seconds;
-  }
-
-  var timeString = hours + ':' + minutes + ':' + seconds;
-  document.getElementById('timer').innerHTML = timeString;
-}
+document.getElementById('iniciarBoton').addEventListener('click', iniciarCronometro);
+document.getElementById('pararBoton').addEventListener('click', pararCronometro);
