b0d3539f31b2e5a79dd351ec8e8275177a08fe34
osernam <osernam@gmail.com>
2023-05-16 09:51:59 -0500

    Inicio adecuación template cronometro

--- cronometro/urls.py
+++ cronometro/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import *
 
+app_name = "cronometro"
 
 urlpatterns = [
     path('index', homeView, name='home'),
-    path('cronometro/', cronometroView, name='cronometro'),
+    path('cronometro/', cronometroView, name= 'cronometro'),
     path('cronometro/tiempo-parcial/', tiempo_parcial, name='tiempo-parcial'),
     path('cronometro/base/', base, name='base'),
 ]
