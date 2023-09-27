392195c4981a51c791560d0fe393535dcc50eca0
osernam <osernam@gmail.com>
2023-09-27 10:06:53 -0500

    render

--- app/settings.py
+++ app/settings.py
@@ -91,9 +91,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
         default='postgresql///postgres:postgres@localhost/postgres',
-        conn_max_age=600
-    )
+        conn_max_age=600)
+    
+    
 }
 
 
