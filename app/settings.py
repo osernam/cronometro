acbc0e734ab18a8e68fd64de67032e5e3c885f06
osernam <osernam@gmail.com>
2023-09-27 08:06:27 -0500

    producción

--- app/settings.py
+++ app/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
+https://www. youtube.com/watch?v=e6PkGDH4wWA
 """
 
 from pathlib import Path
